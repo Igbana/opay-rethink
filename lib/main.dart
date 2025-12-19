ca4c9afe1322b0404f7a3bd73dca1c51e0dc28f3
Igbana <igbanaaondosoo13@gmail.com>
2025-12-19 15:46:51 +0100

    more cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:opay_rethink/homepage.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-      // statusBarIconBrightness: Brightness.dark,
-      // statusBarBrightness: Brightness.light,
-    ),
-  );
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle());
 
   runApp(MaterialApp(home: HomePage()));
 }
